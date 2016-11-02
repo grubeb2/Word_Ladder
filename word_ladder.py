02f6639b29a6d7f97ec2cb14d8b39c976d8358ce
grubeb2 <grubeb2@rpi.edu>
2016-11-02 16:54:02 -0400

    Update word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -4,14 +4,15 @@ import networkx as nx
 def makeGraph():
 	file inputWords = open('words.txt', 'r')
 	wordsList = set()
-	#symbols = '`1234567890-=~!@#$%^&*()_+[]\;'+"',./{}|:"+'"<>?'
 	for line in inputWords.readlines():
 		if line.startswith('*'):
 			continue;
 		line = line.translate(None, '`1234567890-=~!@#$%^&*()_+[]\;'+"',./{}|:"+'"<>?')
 		line = line.lower()
 		wordsList.add(line)
-	
+	G = Graph(name='words')
+	G.add_nodes_from(wordsList)
+		
 
 
 
